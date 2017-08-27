7a4ec26068fbb26c52155c1e077f8a488702639c
rikyuusima <equanz324@gmail.com>
2017-08-27 14:27:27 +0900

    fix overwrite symbolic link

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # make symbolic link
-ln -s $(cd $(dirname $0) && pwd)/.zshrc $HOME/.zshrc
-ln -s $(cd $(dirname $0) && pwd)/.vimrc $HOME/.vimrc
-ln -s $(cd $(dirname $0) && pwd)/.emacs.d/ $HOME/.emacs.d
+ln -nfs $(cd $(dirname $0) && pwd)/.zshrc $HOME/.zshrc
+ln -nfs $(cd $(dirname $0) && pwd)/.vimrc $HOME/.vimrc
+ln -nfs $(cd $(dirname $0) && pwd)/.emacs.d/ $HOME/.emacs.d
 
 # message
 echo "Install Completed"
