c18cbce22ba764e0d1df6d672d394c9de0f43f38
rikyuusima <equanz324@gmail.com>
2017-12-18 09:15:49 +0900

    add sp-pair '「', '」'

--- .emacs.d/conf/smartparens-config-init.el
+++ .emacs.d/conf/smartparens-config-init.el
@@ -2,6 +2,7 @@
 (smartparens-global-mode t)
 
 (sp-pair "<" nil :actions :rem) ;; ignore autocomplete('<', '>')
+(sp-pair "「" "」") ;; pairing ('「', '」')
 
 ;; ignore autoremove pair
 (ad-disable-advice 'delete-backward-char 'before 'sp-delete-pair-advice)
