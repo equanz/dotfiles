69cb0b26d9c98de2cea635ec8493230874d8d2dc
equanz <equanz324@gmail.com>
2019-02-07 13:13:58 +0900

    feat: add Markdown preview command to Emacs

--- .emacs.d/inits/32_markdown.el
+++ .emacs.d/inits/32_markdown.el
@@ -1,4 +1,20 @@
-;; markdown-modeの設定
+;; config of markdown-mode
 (setq markdown-command "marked --gfm")
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-;; ここまで
+
+;; command setting
+(bind-key "C-c p" 'markdown-open-in-firefox-macos)
+
+;; open to Firefox (only use in macOS)
+(defun markdown-open-in-firefox-macos ()
+  "Open path in Firefox (macOS)"
+  (interactive)
+  (message    (mapconcat #'shell-quote-argument
+              (list "open" "-a" "Firefox" buffer-file-name)
+              " "))
+  (async-shell-command
+   (mapconcat #'shell-quote-argument
+              (list "open" "-a" "Firefox" buffer-file-name)
+              " "))
+  )
+;; end of config
