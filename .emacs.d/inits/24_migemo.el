3686887f7ac0795f5462add6d4feaeb214193c1a
equanz <equanz324@gmail.com>
2019-07-14 20:22:45 +0900

    feat: add migemo config

---
+++ .emacs.d/inits/24_migemo.el
@@ -0,0 +1,14 @@
+(require 'migemo)
+
+;; migemo command
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+
+;; migemo dict path
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(migemo-init)
+
