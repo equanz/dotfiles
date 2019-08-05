ec858f604fdec3677a7938b6985653a7bf201c3e
equanz <equanz324@gmail.com>
2019-08-06 07:13:33 +0900

    fix: add diminish config

--- .emacs.d/inits/18_diminish.el
+++ .emacs.d/inits/18_diminish.el
@@ -2,7 +2,7 @@
 
 (diminish 'smartparens-mode)
 (diminish 'undo-tree-mode)
-(diminish 'auto-complete-mode)
+(diminish 'company-mode)
 (diminish 'smooth-scroll-mode)
 (diminish 'emmet-mode)
 (diminish 'global-whitespace-mode)
@@ -14,4 +14,5 @@
 (diminish 'which-key-mode)
 (diminish 'smart-tab-mode)
 (diminish 'eldoc-mode)
+(diminish 'yas-minor-mode)
 
