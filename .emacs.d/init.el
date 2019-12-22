cab8578a20f78005d9cec2d7015b661a11427a08
equanz <equanz324@gmail.com>
2019-12-22 22:48:51 +0900

    chore: add php-mode, refactor: fix neotree config style

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -90,4 +90,4 @@
  '(neo-vc-integration (quote (face char)))
  '(package-selected-packages
    (quote
-    (flycheck yatex let-alist lsp-ui doom-modeline smart-tab ripgrep lsp-mode company-lsp migemo ein dockerfile-mode which-key undo-tree web-mode yaml-mode company calfw howm go-mode bind-key term+mux helm expand-region diminish less-css-mode term+ init-loader exec-path-from-shell typescript-mode atom-one-dark-theme magit markdown-mode smartparens rainbow-mode powerline neotree mozc emmet-mode auto-complete all-the-icons))))
+    (php-mode flycheck yatex let-alist lsp-ui doom-modeline smart-tab ripgrep lsp-mode company-lsp migemo ein dockerfile-mode which-key undo-tree web-mode yaml-mode company calfw howm go-mode bind-key term+mux helm expand-region diminish less-css-mode term+ init-loader exec-path-from-shell typescript-mode atom-one-dark-theme magit markdown-mode smartparens rainbow-mode powerline neotree mozc emmet-mode auto-complete all-the-icons))))
