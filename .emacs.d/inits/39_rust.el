c2e81e197fb44ed3589541442127030d03074cd0
equanz <equanz324@gmail.com>
2022-07-15 01:21:17 +0900

    chore: use rust-analyzer in lsp-mode

--- .emacs.d/inits/39_rust.el
+++ .emacs.d/inits/39_rust.el
@@ -1,6 +1,7 @@
 (require 'rust-mode)
 (add-to-list 'exec-path (expand-file-name "~/.cargo/bin"))
-(setq rust-format-on-save t)
+(setq lsp-rust-server 'rust-analyzer)
 (add-hook 'rust-mode-hook 'cargo-minor-mode)
+(add-hook 'before-save-hook (lambda () (when (eq 'rust-mode major-mode) (lsp-format-buffer))))
 
 (add-hook 'rust-mode-hook 'lsp)
