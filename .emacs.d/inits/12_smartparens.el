1d42766c213ce3b9a171bb4a5a6d7b1aa533256d
equanz <equanz324@gmail.com>
2022-07-17 03:04:25 +0900

    chore: ignore single quote parens when using emacs-lisp-mode

--- .emacs.d/inits/12_smartparens.el
+++ .emacs.d/inits/12_smartparens.el
@@ -2,6 +2,7 @@
   :config
   (sp-pair "<" nil :actions :rem) ;; ignore autocomplete('<', '>')
   (sp-pair "「" "」") ;; pairing ('「', '」')
+  (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
   ;; ignore autoremove pair
   (ad-disable-advice 'delete-backward-char 'before 'sp-delete-pair-advice)
   (ad-activate 'delete-backward-char)
