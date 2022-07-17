a73b557ab7ebf2940c49445800b18abeab513607
equanz <equanz324@gmail.com>
2022-07-17 21:21:34 +0900

    chore: add chef-foodcritic to flycheck-disabled-checkers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -113,5 +113,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flycheck-disabled-checkers '(javascript-jshint javascript-jscs emacs-lisp-checkdoc))
+ '(flycheck-disabled-checkers
+   '(javascript-jshint javascript-jscs emacs-lisp-checkdoc chef-foodcritic))
  '(neo-vc-integration '(face char)))
