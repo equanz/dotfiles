d388111d59de2b78a6be0288f2ba8a74bc4297c0
equanz <equanz324@gmail.com>
2022-12-01 20:25:57 +0900

    chore: disable treemacs-follow-mode

--- .emacs.d/inits/13_project-tree.el
+++ .emacs.d/inits/13_project-tree.el
@@ -8,6 +8,7 @@
   (add-hook 'treemacs-mode-hook #'(lambda () (display-line-numbers-mode -1)))
   :config
   (treemacs-resize-icons 15)
+  (treemacs-follow-mode -1)
   :bind (("C-c o" . treemacs-select-window)
          :map treemacs-mode-map
          ("C-c c" . treemacs-select-directory)
@@ -17,33 +18,3 @@
   :bind (("C-c C-f" . projectile-find-file)
          ("C-c C-d" . projectile-find-dir)))
 (use-package treemacs-projectile)
-
-;; (require 'all-the-icons)
-
-;; (require 'neotree)
-
-;; ;; keymap
-;; (bind-key "<f8>" 'neotree-toggle)
-;; (bind-key* "C-c o" 'neotree-show)
-;; (bind-key "C-u" 'neotree-select-up-and-unfold-node neotree-mode-map)  ;; unfold parent directory
-;; (bind-key "C-c C-o" 'neotree-open-command neotree-mode-map) ;; shell open
-
-;; ;; option
-;; (setq neo-show-hidden-files t)
-;; (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-
-;; (neotree-show)
-
-;; ;; util functions
-;; (defun neotree-select-up-and-unfold-node ()
-;;   "select-up node and unfold"
-;;   (interactive)
-;;   (neotree-select-up-node)
-;;   (neotree-quick-look)
-;; )
-
-;; (defun neotree-open-command ()
-;;   "shell open command"
-;;   (interactive)
-;;   (shell-command "open ./")
-;; )
