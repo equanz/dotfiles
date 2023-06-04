eacf8ca2d1ee335128b17c30fe2fcd9bdde2182f
equanz <equanz324@gmail.com>
2023-06-04 14:06:17 +0900

    chore: set C-j to haskell-indentation-newline-and-indent in haskell-mode-map

--- .emacs.d/inits/42_haskell.el
+++ .emacs.d/inits/42_haskell.el
@@ -3,4 +3,6 @@
   (use-package lsp-haskell
     :init
     (add-hook 'haskell-mode-hook #'lsp-deferred)
-    (add-hook 'haskell-literate-mode-hook #'lsp-deferred)))
+    (add-hook 'haskell-literate-mode-hook #'lsp-deferred))
+  :bind (:map haskell-mode-map
+         ("C-j" . haskell-indentation-newline-and-indent)))
