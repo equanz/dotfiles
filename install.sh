eee172f39180c4c3e4438cc414112622f365e464
equanz <equanz324@gmail.com>
2024-10-30 02:01:57 +0900

    chore: add Emacs build script for Debian

--- install.sh
+++ install.sh
@@ -28,11 +28,13 @@ for dot_path in ${dotfiles_dir}/.??*; do
     fi
 done
 
+DEFAULT_EMACS_VERSION=29.1
+TPM_PATH=${HOME}/.tmux/plugins/tpm
 if [ $(uname -s) = 'Darwin' ]; then
     echo '\n----Darwin specific steps----\n'
 
     # install Homebrew
-    if ! $(builtin command -v brew > /dev/null); then
+    if ! $(which brew > /dev/null); then
         echo 'Install Homebrew'
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     else
@@ -41,16 +43,19 @@ if [ $(uname -s) = 'Darwin' ]; then
 
     # install base commands
     HOMEBREW_NO_AUTO_UPDATE=1 brew install git hub tmux zsh
+    if [ ! -d ${TPM_PATH} ]; then
+        git clone https://github.com/tmux-plugins/tpm.git ${TPM_PATH}
+    fi
 
     # build and install GNU Emacs
     # TODO: split to another file
     if [ ! -d /Applications/Emacs.app ]; then
         echo 'Install GNU Emacs'
 
-        DEFAULT_EMACS_VERSION=29.1
         read -p "Enter GNU Emacs version (default: ${DEFAULT_EMACS_VERSION}): " EMACS_VERSION
         EMACS_VERSION=${EMACS_VERSION:-${DEFAULT_EMACS_VERSION}}
-        pushd ~/Downloads
+        mkdir -p ${HOME}/src
+        pushd ${HOME}/src
 
         # install build dependencies
         HOMEBREW_NO_AUTO_UPDATE=1 brew install autoconf gnutls pkg-config texinfo
@@ -64,7 +69,7 @@ if [ $(uname -s) = 'Darwin' ]; then
 
         # build
         ./autogen.sh
-        ./configure --without-x --with-ns --with-modules --with-xwidgets CC=clang
+        ./configure --without-x --with-ns --with-native-compilation --with-modules --with-xwidgets --with-gif CC=clang
         # use number of cores instead of 8
         make -j8
         make install -j8
@@ -79,19 +84,56 @@ if [ $(uname -s) = 'Darwin' ]; then
     else
         echo 'GNU Emacs is already installed'
     fi
+elif $(which apt > /dev/null); then
+    echo '\n----Debian specific steps----\n'
+
+    # install base commands
+    sudo apt install git hub tmux zsh
+    if [ ! -d ${TPM_PATH} ]; then
+        git clone https://github.com/tmux-plugins/tpm.git ${TPM_PATH}
+    fi
+
+    # build and install GNU Emacs
+    if [ ! -f ${HOME}/usr/bin/emacs ]; then
+        echo 'Install GNU Emacs'
+
+        read -p "Enter GNU Emacs version (default: ${DEFAULT_EMACS_VERSION}): " EMACS_VERSION
+        EMACS_VERSION=${EMACS_VERSION:-${DEFAULT_EMACS_VERSION}}
+        mkdir -p ${HOME}/src
+        pushd ${HOME}/src
+
+        # install build dependencies
+        sudo apt install build-essential autoconf gnutls-bin pkg-config texinfo \
+            libgtk-3-dev libwebkit2gtk-4.0-dev libgnutls28-dev libncurses-dev \
+            libjpeg-dev libgif-dev libtiff-dev libgccjit-9-dev
+
+        # download sources
+        curl -L -o emacs-${EMACS_VERSION}.tar.gz https://ftp.gnu.org/gnu/emacs/emacs-${EMACS_VERSION}.tar.gz
+        tar xzf emacs-${EMACS_VERSION}.tar.gz
+        pushd emacs-${EMACS_VERSION}
+
+        read -p "Apply patches to $(pwd) if needed..."
+
+        EMACS_PREFIX=${HOME}/usr
+        mkdir ${EMACS_PREFIX}
+
+        # build
+        ./autogen.sh
+        ./configure --with-pgtk --with-native-compilation --with-modules --with-xwidgets --with-gif --prefix=${EMACS_PREFIX} CC=gcc
+        # use number of cores instead of 8
+        make -j8
+        make install -j8
+
+        popd
+        popd
+    else
+        echo 'GNU Emacs is already installed'
+    fi
 else
-    echo '\n----non-Darwin specific steps----\nDo nothing'
+    echo '\n----Undefined---\n'
 fi
 
 echo '\n====End of env specific steps====\n'
-
-if [ ! -d ${HOME}/.oh-my-zsh ]; then
-    echo 'Install Oh My Zsh'
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --keep-zshrc"
-    echo 'You should change shell to zsh'
-else
-    echo 'Oh My Zsh is already installed'
-fi
 
 # message
 echo '\n\nInstallation procedures are completed\nHello, world!'
